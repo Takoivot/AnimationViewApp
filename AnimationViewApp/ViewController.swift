00cb15869f688c4c373069d12ce7b00e8f4a66c3
Artur <alferovnet@mail.ru>
2022-05-20 20:23:48 +0300

    setups VC

--- AnimationViewApp/ViewController.swift
+++ AnimationViewApp/ViewController.swift
@@ -5,15 +5,19 @@
 //  Created by Артур Сахбиев on 20.05.2022.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet var animationView: UIView!
+    @IBOutlet var animationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func animationButttonPressed(_ sender: UIButton) {
+    }
+    
 }
 
